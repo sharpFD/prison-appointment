1c0aa1344c91ecb85327c75412e216766a67a91e
zoe <18309288986@163.com>
2017-12-07 22:45:24 +0800

    去除多余的包

--- src/main/java/com/zoe/springboot/SpringbootApplication.java
+++ src/main/java/com/zoe/springboot/SpringbootApplication.java
@@ -1,13 +1,8 @@
 package com.zoe.springboot;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @MapperScan("com.zoe.springboot.dao")
